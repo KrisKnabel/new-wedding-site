b99bc83fc5a48b95e39afab213b93ad42074ca41
Kris Knabel <krisknabel@gmail.com>
2017-07-20 04:59:45 -0700

    All working but RSVP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,65 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'
+
+// Angular Material Animation Module
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Gesture Support for Angular Material
+import 'hammerjs';
+
+// Angular Material Component Modules
+import { MdTabsModule } from '@angular/material';
+
+// Angular Flex Layout
+import { FlexLayoutModule } from '@angular/flex-layout'
+
 
 import { AppComponent } from './app.component';
+import { LocationComponent } from './location/location.component';
+import { RsvpComponent } from './rsvp/rsvp.component';
+import { GiftsComponent } from './gifts/gifts.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'location',
+    component: LocationComponent
+  },
+  {
+    path: 'rsvp',
+    component: RsvpComponent
+  },
+  {
+    path: 'gifts',
+    component: GiftsComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocationComponent,
+    RsvpComponent,
+    GiftsComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdTabsModule,
+    FlexLayoutModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
